521f7677e0d35c42b08d7e22d681eca5c886df41
SheraramPrajapat1998 <sheraramprajapat1998@gmail.com>
2021-01-28 21:36:03 +0530

    app header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import FormControl from "@material-ui/core/FormControl";
+import { Select, MenuItem } from "@material-ui/core";
+
+// https://disease.sh/v3/covid-19/countries
 
 function App() {
-  return (
-    <div>
-      {/* Header with countries */}
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  
+  useEffect(() => {
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((resp) => resp.json())
+        .then((data) => {
+          const countries = data.map(({ country, countryInfo }) => ({
+            name: country,
+            value: countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
 
+  const allCountries = countries.map(({ name, value }) => {
+    return (
+      <MenuItem key={name} value={value}>
+        {name}
+      </MenuItem>
+    );
+  });
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
+  return (
+    <div className="app">
+      <div className="app__header">
+        {/* Header with countries */}
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {allCountries}
+          </Select>
+        </FormControl>
+      </div>
       {/* InfoBoxes */}
 
       {/* Map */}
@@ -14,7 +57,7 @@ function App() {
 
       {/* Graph */}
     </div>
-  )
+  );
 }
 
 export default App;
